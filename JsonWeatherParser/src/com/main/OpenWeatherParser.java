a047654de85aa89dffc3381f8de38207abbd9184
Tobias Bockrath <tobiasbockrath@googlemail.com>
2014-02-27 16:49:21 +0100

    First Commit

---
+++ JsonWeatherParser/src/com/main/OpenWeatherParser.java
@@ -0,0 +1,139 @@
+package com.main;
+
+import java.io.IOException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.io.IOUtils;
+import org.apache.flume.Event;
+import org.apache.flume.EventDrivenSource;
+import org.apache.flume.channel.ChannelProcessor;
+import org.apache.flume.event.EventBuilder;
+import org.apache.flume.source.AbstractSource;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
+import com.objects.Weather;
+
+public class OpenWeatherParser extends AbstractSource implements EventDrivenSource  {
+
+	private static String [] citys = {"Amsterdam", "Ankara", "Andorra la Vella", "Athen", "Belgrad", "Berlin", "Bern",
+										"Bratislava", "Brüssel", "Budapest", "Chișinău", "San Marino",
+										"Dublin, irl", "Helsinki", "Kiew", "Kopenhagen ", "Lissabon", "Ljubljana", "London,uk",
+										"Luxemburg", "Madrid", "Minsk", "Monaco ", "Moskau", "Nikosia", "Oslo","Paris,fr",
+										"Podgorica", "Prag", "Reykjavík", "Riga ", "Rom", "Sarajevo", "Skopje",
+										"Sofia", "Stockholm", "Talinn", "Tirana", "Vaduz", "Valletta", "Vilnius",
+										"Warschau", "Wien", "Zagreb"};
+	
+	private static String weatherJsonObject;
+	
+	
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+		//wird stündlich ausgeführt
+		while(true){
+			
+			final ChannelProcessor channel = getChannelProcessor();
+			
+			//Einheitlicher Zeitpunkt an dem die Daten abgerufen wurden
+			Date aDate = new Date( );
+		    SimpleDateFormat sdt = new SimpleDateFormat ("yyyy.MM.dd HH:mm:ss");
+		    String dt =  sdt.format(aDate);
+			
+		    //ruft die Daten aller Städte ab
+			for (String city: citys){
+				
+				String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city +"&mode=json&units=metric&lang=de";
+				
+				final Map<String, String> headers = new HashMap<String, String>();
+				Event event = null;
+				Weather aWeather = new Weather();
+				
+				//JSON holen und auswerten
+				try {
+			
+				weatherJsonObject = IOUtils.toString(new URL(url));
+				JSONObject obj1 = (JSONObject) JSONValue.parseWithException(weatherJsonObject);
+				
+					if (!obj1.isEmpty()){
+						
+						JSONObject obj2 = (JSONObject) obj1.get("coord");	
+						JSONObject obj3 = (JSONObject) obj1.get("sys");
+						JSONObject obj4 = (JSONObject) obj1.get("main");
+						JSONObject obj5 = (JSONObject) obj1.get("wind");						
+						
+						aWeather.setId(obj1.get("id").toString());
+						aWeather.setDt(obj1.get("dt").toString());
+						aWeather.setName(obj1.get("name").toString());
+						
+						//coord
+						if (!obj2.isEmpty()){
+						
+							aWeather.setLon(obj2.get("lon").toString());
+							aWeather.setLat(obj2.get("lat").toString());
+						}
+						
+						//sys
+						if(!obj3.isEmpty()){
+							aWeather.setCountry(obj3.get("country").toString());
+							aWeather.setSunrise(obj3.get("sunrise").toString());
+							aWeather.setSunset(obj3.get("sunset").toString());	
+						}
+						
+						//main
+						if (!obj4.isEmpty()){
+							
+							aWeather.setTemp(obj4.get("temp").toString());
+							aWeather.setHumidity(obj4.get("humidity").toString());
+							aWeather.setPressure(obj4.get("pressure").toString());
+							aWeather.setTemp_min(obj4.get("temp_min").toString());
+							aWeather.setTemp_max(obj4.get("temp_max").toString());
+						}
+			
+						//wind
+						if (!obj5.isEmpty()){
+							
+							aWeather.setSpeed(obj5.get("speed").toString());
+							aWeather.setDeg(obj5.get("deg").toString());
+						}
+					}
+
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();		
+				}	
+				
+
+				//body zusammenbauen
+				String body = "time: " + dt + "; " + "ort: " + aWeather.getName() + "; " + aWeather.getCountry() + "; "
+							+ "sunrise: " + aWeather.getSunrise() + "; " + "sunset: " + aWeather.getSunset() + "; " 
+							+ "temp: " +aWeather.getTemp() + "; " + "tempmax: " + aWeather.getTemp_max() + "; " 
+							+ "tempmin: " + aWeather.getTemp_min() + "; " + "humidity: " + aWeather.getHumidity() + "; "
+							+ "pressure: " + aWeather.getPressure() + "; " + "speed: " + aWeather.getSpeed();
+				
+				//header zusammenbauen
+				long now = System.currentTimeMillis();
+				headers.put("timestamp", Long.toString(now));
+				
+				//event generieren und an den Channel übergeben
+				event = EventBuilder.withBody(body.getBytes(), headers);
+		        channel.processEvent(event);
+				
+			}
+	        
+	        try {
+				Thread.sleep(3600000); // 1 Stunde schlafen: 
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ JsonWeatherParser/src/com/objects/Weather.java
@@ -0,0 +1,192 @@
+package com.objects;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Weather {
+
+	private String id;
+	private String dt;
+	private String name;
+	private String lon;
+	private String lat;
+	private String temp;
+	private String humidity;
+	private String pressure;
+	private String temp_min;
+	private String temp_max;
+	private String speed;
+	private String deg;
+	private String country;
+	private String sunrise;
+	private String sunset;
+
+	
+	public Weather(){
+		
+	}
+	
+	public String convertUnixTimeStamp(String unixTimeStamp){
+		
+		TimeZone.setDefault(TimeZone.getTimeZone("CET"));
+		Date aDate = new Date ();
+		aDate.setTime((Long.parseLong(unixTimeStamp))*1000);
+		
+	    SimpleDateFormat sdt = new SimpleDateFormat ("yyyy.MM.dd HH:mm:ss");
+	    String convertedDate =  sdt.format(aDate);
+		
+		return convertedDate;
+	}
+	
+	
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getDt() {
+		return dt;
+	}
+
+
+	public void setDt(String dt) {
+		this.dt = dt;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getLon() {
+		return lon;
+	}
+
+
+	public void setLon(String lon) {
+		this.lon = lon;
+	}
+
+
+	public String getLat() {
+		return lat;
+	}
+
+
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+
+
+	public String getTemp() {
+		return temp;
+	}
+
+
+	public void setTemp(String temp) {
+		this.temp = temp;
+	}
+
+
+	public String getHumidity() {
+		return humidity;
+	}
+
+
+	public void setHumidity(String humidity) {
+		this.humidity = humidity;
+	}
+
+
+	public String getPressure() {
+		return pressure;
+	}
+
+
+	public void setPressure(String pressure) {
+		this.pressure = pressure;
+	}
+
+
+	public String getTemp_min() {
+		return temp_min;
+	}
+
+
+	public void setTemp_min(String temp_min) {
+		this.temp_min = temp_min;
+	}
+
+
+	public String getTemp_max() {
+		return temp_max;
+	}
+
+
+	public void setTemp_max(String temp_max) {
+		this.temp_max = temp_max;
+	}
+
+
+	public String getSpeed() {
+		return speed;
+	}
+
+
+	public void setSpeed(String speed) {
+		this.speed = speed;
+	}
+
+
+	public String getDeg() {
+		return deg;
+	}
+
+
+	public void setDeg(String deg) {
+		this.deg = deg;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+	public String getSunrise() {
+		return sunrise;
+	}
+
+
+	public void setSunrise(String sunrise) {
+		this.sunrise = convertUnixTimeStamp(sunrise);
+	}
+
+
+	public String getSunset() {
+		return sunset;
+	}
+
+
+	public void setSunset(String sunset) {
+		this.sunset = convertUnixTimeStamp(sunset);
+	}
+
+}
